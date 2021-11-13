fca5211fd225538d244f089e2345d2b14d806704
axmeshkov <axmeshkov@gmail.com>
2021-11-13 05:16:53 +0300

    added config file

---
+++ config/init.php
@@ -0,0 +1,19 @@
+<?php
+
+define("DEBUG", 1);
+
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/php_my_framework/core');
+define("LIBS", ROOT . '/vendor/php_my_framework/core/libc');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONF", ROOT . '/config');
+define("LAYOUT", 'default');
+
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+require_once ROOT . '/vendor/autoload.php';
