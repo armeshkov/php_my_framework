a85b84b47c50a2d736ddcd1dd6c27151088fc3c3
axmeshkov <axmeshkov@gmail.com>
2021-11-14 03:52:38 +0300

    added errors handling

--- public/index.php
+++ public/index.php
@@ -3,4 +3,5 @@ require_once '/var/www/config/init.php';
 require_once LIBS . '/functions.php';
 
 new \php_my_framework\App();
-debug(\php_my_framework\App::$app->getProperties());
+
+throw new Exception('Страница не найдена', 500);
