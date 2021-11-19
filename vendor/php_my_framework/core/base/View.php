53ce4bd5c327f6457cbab0d8094133a6b51b1d63
axmeshkov <axmeshkov@gmail.com>
2021-11-19 04:35:10 +0300

    fixed layout in construct and implemented method render

--- vendor/php_my_framework/core/base/View.php
+++ vendor/php_my_framework/core/base/View.php
@@ -9,25 +9,50 @@ class View
     public $model;
     public $view;
     public $prefix;
-    public $layout
+    public $layout;
     public $data = [];
     public $meta = [];
 
-    public function __construct($route, $layout = '', $view = '', $meta) {
+    public function __construct($route, $meta, $layout = '', $view = '') {
         $this->route = $route;
         $this->controller = $route['controller'];
         $this->model = $route['controller'];
         $this->view = $view;
         $this->public = $route['prefix'];
         $this->meta = $meta;
-        if ($layout) {
-            if ($layout === fasle) {
-                $this->layout = false;
+        if ($layout === false) {
+            $this->layout = false;
+        } else {
+            $this->layout = $layout ?: LAYOUT;
+        }
+
+    }
+
+    public function render($data) {
+        $viewFile = APP . "/views/{$this->prefix}{$this->controller}/{$this->view}.php";
+        if (is_file($viewFile)) {
+            ob_start();
+            require_once $viewFile;
+            $content = ob_get_clean();
+        } else {
+            throw  new \Exception("Не найден вид {$viewFile}", 500);
+        }
+
+        if ($this->layout !== false) {
+            $layoutFile = APP . "/views/layouts/{$this->layout}.php";
+            if (is_file($layoutFile)) {
+                require_once $layoutFile;
             } else {
-                $this->layout = $layout ?: LAYOUT;
+                throw  new \Exception("Не найден шаблон {$this->layout}", 500);
             }
         }
     }
 
+    public function getMeta() {
+        $output = 'title' . $this->meta['titile'] . '</title>' . PHP_EOL;
+        $output .= '<meta name="discription" content="' . $this->meta['disc'] . '">' . PHP_EOL;
+        $output .= '<meta name="keywords" content="' . $this->meta['keywords'] . '">' . PHP_EOL;
+        return $output;
+    }
 
 }
